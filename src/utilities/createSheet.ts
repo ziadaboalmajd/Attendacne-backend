789a6e2c59ff9dd4efe9e45da601f602a2c41a64
Ziad Aboalmjd <ziadgamalaboalmajd@gmail.com>
2022-12-11 01:18:49 +0200

    nLast

--- src/utilities/createSheet.ts
+++ src/utilities/createSheet.ts
@@ -16,14 +16,14 @@ function createSheet(respond: any, finalArduinoD: any, DrName: any, today: strin
     ];
 
     const settings: ISettings = {
-        fileName: (courseName + " " + today), // Name of the resulting spreadsheet
+        //fileName: (courseName + " " + today), // Name of the resulting spreadsheet
         extraLength: 3, // A bigger number means that columns will be wider
-        //writeMode: 'writeFile', // The available parameters are 'WriteFile' and 'write'. This setting is optional. Useful in such cases https://docs.sheetjs.com/docs/solutions/output#example-remote-file
         writeOptions: {
             type: "buffer",
             bookType: "xlsx"
         }, // Style options from https://github.com/SheetJS/sheetjs#writing-options
         RTL: true, // Display the columns from right-to-left (the default value is false)
+        //writeMode: 'writeFile', // The available parameters are 'WriteFile' and 'write'. This setting is optional. Useful in such cases https://docs.sheetjs.com/docs/solutions/output#example-remote-file
         //file: ""
     };
 
@@ -38,29 +38,3 @@ function createSheet(respond: any, finalArduinoD: any, DrName: any, today: strin
 };
 
 export default { createSheet };
-
-
-
-
-
-
-
-
-/*
-const callback = function (sheet) {
-    console.log("Download complete:", sheet);
-};
-
-  xlsx(data, settings, callback); // Will download the excel file
-*/
-
-/* generate Buffer 
-const buf = XLSX.write(wb, { type:"buffer", bookType:"xlsx" });
-
-/* prepare response headers 
-res.statusCode = 200;
-res.setHeader('Content-Disposition', 'attachment; filename="SheetJSNode.xlsx"');
-res.setHeader('Content-Type', 'application/vnd.ms-excel');
-res.end(buf);
-
- */
